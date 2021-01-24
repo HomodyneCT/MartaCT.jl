7696bbfd45df34ef7fe4354de4a83dc62e5693da
Nicola Mosco <nicola.mosco@gmail.com>
2021-01-24 01:24:31 +0100

    Readded deploydocs for github. Added docs generation script for gitlab

--- docs/make_gitlab.jl
+++ docs/make_gitlab.jl
@@ -5,9 +5,9 @@ makedocs(
     sitename = "Marta",
     format = Documenter.HTML(),
     modules = [Marta]
-)
+))
 
 deploydocs(
-    repo = "github.com/HomodyneCT/Marta.git",
-    deploy_config = Documenter.GitHubActions()
+    repo = "gitlab.com/homodyne-ct/Marta.git",
+    deploy_config = Documenter.GitLab()
 )
