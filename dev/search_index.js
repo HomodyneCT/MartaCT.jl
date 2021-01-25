ead1a70dc1c0aa5e0b07675de3e010e9cf1af7a0
Documenter.jl <documenter@juliadocs.github.io>
2021-01-25 17:52:45 +0000

    build based on bd8f1a2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#Marta","page":"Marta","title":"Marta","text":"","category":"section"},{"location":"","page":"Marta","title":"Marta","text":"Documentation for Marta.","category":"page"},{"location":"","page":"Marta","title":"Marta","text":"Modules = [Marta.CTImages, Marta.CTIO, Marta.CTScan, Marta.CTPlot]","category":"page"}]
+}
