705a3f96808cebbf4e886060821ba6064bedeb13
Nicola Mosco <nicola.mosco@gmail.com>
2021-02-27 00:09:35 +0100

    linspace defined for IntervalSets

--- src/basic_definitions.jl
+++ src/basic_definitions.jl
@@ -3,17 +3,34 @@ export datatype, linspace
 function datatype end
 function _atype end
 
-function linspace(start::T, stop::U, len::Integer) where {
+@inline function linspace(start::T, stop::U, len::Integer) where {
     T <: Number,
     U <: Number,
 }
     range(start, stop, length = len)
 end
 
-function linspace(::Type{T}, start::X, stop::Y, len::Integer) where {
+@inline function linspace(::Type{T}, start::X, stop::Y, len::Integer) where {
     T <: Number,
     X <: Number,
     Y <: Number,
 }
     range(T(start), T(stop), length = len)
+end
+
+@inline function linspace(
+    i::IntervalSets.ClosedInterval{T},
+    len::Integer,
+) where {T <: Number}
+    a, b = IntervalSets.endpoints(i)
+    linspace(a, b, len)
+end
+
+@inline function linspace(
+    ::Type{T},
+    i::IntervalSets.ClosedInterval{U},
+    len::Integer
+) where {T <: Number,U <: Number}
+    a, b = IntervalSets.endpoints(i)
+    linspace(T, a, b, len)
 end
