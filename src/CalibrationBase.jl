3fc7a55811461325e0045fc27518680f9f4c0447
Nicola Mosco <nicola.mosco@gmail.com>
2021-03-18 20:14:33 +0100

    Fix kwargs

--- src/CalibrationBase.jl
+++ src/CalibrationBase.jl
@@ -14,9 +14,9 @@ function calibrate_tomogram end
 for nm ∈ (:calibrate_image, :calibrate_tomogram)
     @eval begin
         $nm(; kwargs...) = x -> $nm(x; kwargs...)
-        @traitfn $nm(m::M; kwargs) where {M; Monad{M}} =
+        @traitfn $nm(m::M; kwargs...) where {M; Monad{M}} =
             mmap($nm(; kwargs...), m)
-        @traitfn $nm(m::M, x; kwargs) where {M; Monad{M}} =
+        @traitfn $nm(m::M, x; kwargs...) where {M; Monad{M}} =
             mmap($nm(x; kwargs...), m)
     end
 end
