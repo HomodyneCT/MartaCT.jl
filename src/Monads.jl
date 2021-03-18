dfab550d341adfc5a046621d732ee05ee67e80fa
Nicola Mosco <nicola.mosco@gmail.com>
2021-03-18 20:17:15 +0100

    Monads now use traits

--- src/Monads.jl
+++ src/Monads.jl
@@ -14,15 +14,16 @@ function mbind end
 function mreturn end
 function mjoin end
 
-@traitfn mreturn(::Type{M}, x) where {M; Monad{M}} = M(x)
-@traitfn mjoin(m::M) where {M; Monad{M}} = mbind(identity, m)
-@traitfn mbind(f::F, m::M) where {F,M; Callable{F},Monad{M}} = f(mjoin(m))
-@traitfn function mmap(f::F, m::M) where {F,M; Callable{F},Monad{M}}
+@traitfn @inline mreturn(::Type{M}, x) where {M; Monad{M}} = M(x)
+@traitfn @inline mjoin(m::M) where {M; Monad{M}} = mbind(identity, m)
+@traitfn @inline mbind(f::F, m::M) where {F,M; Callable{F},Monad{M}} =
+    f(mjoin(m))
+@traitfn @inline function mmap(f::F, m::M) where {F,M; Callable{F},Monad{M}}
     mbind(m) do x
         mreturn(M, f(x))
     end
 end
-@traitfn ↣(m::M, f::F) where {M,F; Monad{M},Callable{F}} = mbind(f, m)
+@traitfn @inline ↣(m::M, f::F) where {M,F; Monad{M},Callable{F}} = mbind(f, m)
 
 const Optional{T} = Union{Nothing,T}
 const Maybe{T} = Optional{Some{T}}
@@ -35,12 +36,12 @@ Maybe(::Nothing) = nothing
 Maybe(x::T) where {T} = Maybe{T}(x)
 Maybe(m::Maybe) = m
 
-@traitfn mbind(f::F, m::Maybe) where {F; Callable{F}} =
+@traitfn @inline mbind(f::F, m::Maybe) where {F; Callable{F}} =
     isnothing(m) ? nothing : f(something(m))
 
-maybe(b, m::Optional) = isnothing(m) ? b : m
-maybe(b) = m::Optional -> maybe(b, m)
-@traitfn maybe(f::F, b, m::Optional) where {F; Callable{F}} =
+@inline maybe(b, m::Optional) = isnothing(m) ? b : m
+@inline maybe(b) = m::Optional -> maybe(b, m)
+@traitfn @inline maybe(f::F, b, m::Optional) where {F; Callable{F}} =
     maybe(b, Maybe(m) ↣ f)
 
 end # module
