5a1537684bdf1ba987ae37291ce3a8dd3dc16600
Nicola Mosco <nicola.mosco@gmail.com>
2023-04-25 11:35:29 +0200

    Fix symbol not found

--- src/precompile.jl
+++ src/precompile.jl
@@ -1,15 +1,15 @@
-using SnoopPrecompile
+using PrecompileTools
 
-@precompile_setup begin
-    const _pre_types = (:Float32, :Float64)
-    const width = 5
-    const nϕ = 11
+@setup_workload begin
+    _pre_types = (:Float32, :Float64)
+    width = 5
+    nϕ = 11
 
-    @precompile_all_calls begin
+    @compile_workload begin
         for T ∈ _pre_types, Img ∈ CTTestImages._gs_images, G ∈ Geometry._geometry_names, A ∈ (:Radon, :RadonSquare), F ∈ (:FBP, :FBPAFFT, :FBPAFFTSquare, :FBPFFTSquare)
             @eval begin
-                gs = $Img($T; width)
-                g = $G(gs; nϕ)
+                gs = $Img($T; width=$width)
+                g = $G(gs; nϕ=$nϕ)
                 ct = project_image(gs, g, $A()) |> reconstruct_image($F())
                 ctc = calibrate_tomogram(ct, gs)
             end
